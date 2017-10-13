e92093182bc823c627302caabe4653fab2cc5ee4
reflexit <reflexit@users.noreply.github.com>
2017-10-13 20:21:29 +0800

    minor fix: ensure the converted type is correct

--- conbine.py
+++ conbine.py
@@ -15,8 +15,8 @@ def add_dynamic():
     for item in dnm:
         comments_count = str(item['comment_count'])
         score = str(item['good_rate'])
-        current_price = str(item['price']['present_price'])
-        product_id = str(item['product_id'])
+        current_price = safe_convert(item['price']['present_price'])
+        product_id = safe_convert(item['product_id'])
         
         # ensure there are no duplicates
         cursor.execute("select count(*) from jdtest where product_id = '%s' and current_price = '%s' and comments_count = '%s' and score = '%s'" \
@@ -30,16 +30,16 @@ def add_dynamic():
 def add_static():
     stt = list(db.statistic.find())
     for item in stt:
-        product_id = str(item['product_id'])
+        product_id = safe_convert(item['product_id'])
         name = safe_convert(item['goods_name'])
-        params = json.dumps(item['specifications'], ensure_ascii = False, encoding = 'utf-8')
+        params = safe_convert(json.dumps(item['specifications'], ensure_ascii = False, encoding = 'utf-8'))
         brand = safe_convert(item['brand_name'])
         categories = safe_convert(item['classify'])
         description = safe_convert(item['introduce_info'])
         shop_name = safe_convert(item['shop_name'])
-        url = str(item['website'])
+        url = safe_convert(item['website'])
         keyowrd = safe_convert(item['keyword'])
-        images = str(item['image_urls'])
+        images = safe_convert(item['image_urls'])
 
         # check if product_id exists
         cursor.execute("select count(*) from jdtest where product_id = '%s'" % product_id)
@@ -53,14 +53,14 @@ values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '
             print "update static for pid = %s" % product_id
             cursor.execute("update jdtest set name = '%s', params = '%s', brand = '%s', categories = '%s', product_sku = '%s', product_sku_detail = '%s', shop_name = '%s', url = '%s', keyowrd = '%s', images = '%s', detail = '%s', description = '%s' where product_id = '%s'" \
                            % (name, params, brand, categories, product_id, description, shop_name, url, keyowrd, images, description, description, product_id))
-        
 
 def safe_convert(raw):
-    if (type(raw) == type(u'')):
+    if (type(raw) == type(u'') or type(raw) == type('')):
         return raw.replace("'", "''")
     elif (type(raw) == type(None)):
         return u''
-    else:
+    else:   # should never reach here
+        print "ERROR converting", type(raw)
         sys.exit(1)
 
 def main():
